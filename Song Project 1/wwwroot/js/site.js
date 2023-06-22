eb1b59d442241d4aab142f7bba621b8480f0b257
jedfran <jedofrancisco12@gmail.com>
2023-06-22 13:41:24 -0500

    First Commit

---
+++ Song Project 1/wwwroot/js/site.js
@@ -0,0 +1,23 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+//Animation Function
+
+function myAnimation() {
+    let id = null;
+    const elem = document.getElementById("video_animation");
+    let pos = 0;
+    elem.style.display = 'block'
+    clearInterval(id);
+    id = setInterval(frame, 5);
+    function frame() {
+        if (pos == 350) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.left = pos + "px";
+        }
+    }
+}
